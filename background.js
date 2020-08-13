dcd7c96c832a9ea2242f41f4fd7f4fa4723b97f4
NDevTK <31563761+NDevTK@users.noreply.github.com>
2020-08-13 14:59:42 +0100

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.tabs.onUpdated.addListener( // On tab update
   function(tabId, changeInfo, tab) {
-    if (changeInfo.url) { // If URL changed message ContentScript the video id
+    if (changeInfo.url) { // If URL changed message the ContentScript
       chrome.tabs.sendMessage( tabId, {
         message: 'update'
       })
